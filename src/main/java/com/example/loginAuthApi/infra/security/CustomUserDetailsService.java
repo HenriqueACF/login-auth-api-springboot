21d42e63dc6c1af6f02ca11c56759567967a0f1a
henriqueacf <henrique.assis145@gmail.com>
2024-07-26 10:36:47 -0300

    FEAT: (Infra) -> criado CustomUserDetailsService

---
+++ src/main/java/com/example/loginAuthApi/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.loginAuthApi.infra.security;
+
+import com.example.loginAuthApi.domain.user.User;
+import com.example.loginAuthApi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       User user = this.repository.findByEmail(username)
+               .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+       return new org.springframework.security.core.userdetails.User(user.getName(),
+               user.getPassword(),
+               new ArrayList<>());
+    }
+}
